00cfd5d235747a8a59a31d49069241a7cb6c93f3
zero22Dev1 <zero22dev@gmail.com>
2025-06-08 20:02:14 +0900

    プロジェクト ファイルを追加します。

---
+++ statusLabelProject/Form1.Designer.vb
@@ -0,0 +1,74 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        ProgressBar1 = New ProgressBar()
+        Label1 = New Label()
+        Button1 = New Button()
+        SuspendLayout()
+        ' 
+        ' ProgressBar1
+        ' 
+        ProgressBar1.Location = New Point(147, 226)
+        ProgressBar1.Name = "ProgressBar1"
+        ProgressBar1.RightToLeft = RightToLeft.No
+        ProgressBar1.Size = New Size(524, 118)
+        ProgressBar1.TabIndex = 0
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(154, 74)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(63, 25)
+        Label1.TabIndex = 1
+        Label1.Text = "Label1"
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(309, 51)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(339, 100)
+        Button1.TabIndex = 2
+        Button1.Text = "Button1"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10.0F, 25.0F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Button1)
+        Controls.Add(Label1)
+        Controls.Add(ProgressBar1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+
+End Class
---
+++ statusLabelProject/Form1.vb
@@ -0,0 +1,31 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' 初期設定
+        ProgressBar1.Minimum = 0
+        ProgressBar1.Maximum = 100
+        ProgressBar1.Value = 0
+        Label1.Text = "0%"
+    End Sub
+
+    ' ボタンを押したらDBから値を取得して表示
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ' Dim progressValue As Integer = GetProgressFromDatabase()
+
+        ' 安全に範囲内で設定
+        If progressValue >= ProgressBar1.Minimum AndAlso progressValue <= ProgressBar1.Maximum Then
+            SetProgress(progressValue)
+        Else
+            MessageBox.Show("取得した値が範囲外です。")
+        End If
+    End Sub
+
+    ' 進捗バーとラベルを更新する関数
+    Private Sub SetProgress(value As Integer)
+        ProgressBar1.Value = value
+        Label1.Text = value.ToString() & "%"
+    End Sub
+
+End Class
